2da2808281c3aa66bff76a165dba0b5edfcd3846
Amit Patel <amitpatel_7@msn.com>
2018-09-16 21:44:08 -0700

    requested geoJSON data and logged to console

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-const getData = () => console.log("test");
+const getData = () => {
+    d3.json("https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json", (error, dataset) => {
+        if(error) console.log(error);
+        console.log(dataset);
+    });
+};
